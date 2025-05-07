0642eb336b524e6a637fb750dd4a5950c418f17f
Sadiq <sadiqict63@gmail.com>
2025-05-07 12:11:53 +0600

    server done

--- Modules/Core_Modules/HTTP_Modules/server.js
+++ Modules/Core_Modules/HTTP_Modules/server.js
@@ -1,4 +1,10 @@
 const run_Server =require ('http');
 // console.log(run_Server);
 
-run_Server.createServer()
+const port= 3000;
+const hostname='127.0.0.1';
+
+const Server= run_Server.createServer((request, response)=>
+     response.end('<h1> Yahoooooo!!!!!!! server is running </h1>')
+);
+Server.listen(port, hostname, ()=>console.log(`Server is working at http://${hostname}:${port}`) );
